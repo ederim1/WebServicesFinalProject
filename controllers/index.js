8fc78b6116b2abff226d617aa7a623335fb52f4c
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-06-23 11:00:24 -0600

    fixing the result body 2

--- controllers/index.js
+++ controllers/index.js
@@ -12,7 +12,7 @@ controller.getAllReminders = async(req, res) => {
         console.log(`after I send the get to mongo this is what I get back ${reminders}`)
         console.log(`stringify version ${reminders}`)
 
-        res.status(200).json(JSON.stringify(reminders));
+        res.status(200).json(reminders);
     } catch (err) {
         res.status(500).json({message: err.message})
     }
