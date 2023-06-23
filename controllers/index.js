f743b31e47ecd289539991ba20a8b6bba35020a4
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-06-22 22:08:15 -0600

    trying to do the get by user

--- controllers/index.js
+++ controllers/index.js
@@ -9,7 +9,7 @@ const controller = {}
 controller.getAllReminders = async(req, res) => {
     try {
         const reminders = await model.find({"user.email[0]": req.oidc.user.email});
-        res.status(200).json(reminders);
+        res.status(200).json(JSON.stringify(reminders));
     } catch (err) {
         res.status(500).json({message: err.message})
     }
