7da656bfd8354eef3b544ed7f60e637c83dc05b6
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-07-06 09:15:29 -0600

    test 2 working

--- _tests_/reminders-route.test.js
+++ _tests_/reminders-route.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../server.js');
 const jwt = require('jsonwebtoken');
 const env = require('dotenv').config();
+const controller = require('../controllers/index.js');
 
 let server;
 beforeAll((done) => {
@@ -22,12 +23,22 @@ describe('REMINDERS API - Routes Test', () => {
     // expect(Array.isArray(response.body.reminders)).toBe(true);
   });
 
-  it('the id should be a string to getReminderById', async () => {
+  it('should return a reminder by ID', async () => {
     const id = '6494e422b75d97a8b9518e33';
-    const response = await request(app)
-      .get(`/${id}`);
-    expect(typeof response.body.id).toBe('string');
+    const req = { params: { id } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const next = jest.fn();
+  
+    await controller.getReminder(req, res, next);
+  
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(expect.any(Object));
   });
+  
+  
   // Add more test cases for other routes (get reminder by id, post new reminder, edit reminder, delete reminder)
 });
 
