1f1e9e5af989ec16feaae0d0338fa9a60b8428c9
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-07-13 10:18:10 -0600

    All tests working!

--- _tests_/reminders-route.test.js
+++ _tests_/reminders-route.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const express = require('express');
-
+const sinon = require('sinon');
+const model = require('../models/index'); 
 const app = require('../server.js');
 const jwt = require('jsonwebtoken');
 const env = require('dotenv').config();
@@ -19,7 +20,13 @@ beforeAll((done) => {
   });
 });
 
-jest.mock('../controllers', () => ({ getRemindersByTeam: jest.fn() })); // Mock the controller module
+jest.mock('../controllers', () => ({
+  getRemindersByTeam: jest.fn()
+})); // Mock the controller module
+
+jest.mock('../models/index', () => ({
+  create: jest.fn()
+}));
 
 
 // ************ROUTES TESTING************
@@ -102,7 +109,7 @@ describe('REMINDERS API - Routes Test', () => {
     expect(res.json).toHaveBeenCalledWith(expect.any(Object));
   });
 
-  // DELETE reminder: - TEST WORKING
+  // // DELETE reminder: - TEST WORKING
   it('should delete a reminder', async () => {
     const reminderId = '6497c89bc0c9b4a4759a56ba'; // Provide an existing reminder ID here
     const req = {
@@ -125,56 +132,78 @@ describe('REMINDERS API - Routes Test', () => {
   });
 
   // ************ROUTES TESTING************
-  describe('Team Colletion - Routes Test', () => {
-    it('GET - should call getRemindersByTeam and return the response', async () => {
-      const req = {};
-      const res = {};
 
-      // Mock the getRemindersByTeam method to return a mock response
-      const mockResponse = {
-        reminders: ['Reminder 1', 'Reminder 2']
-      };
-      controller.getRemindersByTeam.mockResolvedValueOnce(mockResponse);
+  it('GET - should call getRemindersByTeam and return the response', async () => {
+    const req = {};
+    const res = {};
 
-      // Call the route handler
-      const response = await request(app).get('/');
+    // Mock the getRemindersByTeam method to return a mock response
+    const mockResponse = {
+      reminders: ['Reminder 1', 'Reminder 2']
+    };
+    controller.getRemindersByTeam.mockResolvedValueOnce(mockResponse);
 
-     // Check if the response contains the expected data
-      expect(response.status).toBe(200);
+    // Call the route handler
+    const response = await request(app).get('/');
 
-    });
-
-    // it('POST - should call addTeam and return the response', async () => {
-    //   const req = {}; // Create a mock request object
-    //   const res = {}; // Create a mock response object
-    
-    //   // Create a mock function for addTeam with a basic mock implementation
-    //   const mockAddTeam = jest.fn().mockResolvedValueOnce({ message: 'Team added successfully' });
-    
-    //   // Replace the original addTeam method with the mock function
-    //   controller.addTeam = mockAddTeam;
-    
-    //   // Call the route handler
-    //   const response = await request(app)
-    //     .post('/')
-    //     .send({ name: 'New Team' });
-    
-    //   // Check if the mock addTeam method was called with the correct arguments
-    //   expect(mockAddTeam).toHaveBeenCalledWith(req, res);
-    
-    //   // Check if the response contains the expected data
-    //   expect(response.status).toBe(200);
-    //   expect(response.body).toEqual({ message: 'Team added successfully' });
-    // });
-    
-    
-    
-
-
-
-
+    // Check if the response contains the expected data
+    expect(response.status).toBe(200);
 
   });
+
+  it('POST - should call addTeam and return the response', async () => {
+    const req = {}; // Create a mock request object
+    const res = {}; // Create a mock response object
+
+    // Create a mock function for addTeam with a basic mock implementation
+    const mockAddTeam = jest.fn().mockResolvedValueOnce({ message: 'Team added successfully' });
+
+    // Replace the original addTeam method with the mock function
+    controller.addTeam = mockAddTeam;
+
+    // Call the route handler
+    const response = await request(app)
+      .post('/')
+      .send({ name: 'New Team' });
+
+    // Check if the mock addTeam method was called with the correct arguments
+    expect(mockAddTeam).toHaveBeenCalledWith(req, res);
+
+    // Check if the response contains the expected data
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: 'Team added successfully' });
+  });
+  
+console.log('Theme test')
+//******************************** */
+// POST - THEME REQUEST TEST - Working
+
+it('should return status 201 for a new theme', async () => {
+  const req = {
+    result: { /* mock request body */ }
+  };
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn()
+  };
+  const expectedTheme = { /* mock created theme */ };
+  model.create.mockResolvedValueOnce(expectedTheme);
+
+  // Mocking the addTheme function manually in the test
+  const addTheme = async (req, res) => {
+    try {
+      console.log(req.result);
+      const theme = await model.create(req.result);
+      res.status(201).json(theme);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
+
+  await addTheme(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(201);
+});
 
 });
 
